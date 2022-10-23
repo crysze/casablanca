115209420f60ea8ed7c7843bd62dfea71a090013
crysze <christina.stoll@gmx.at>
2022-10-23 13:52:06 +0200

    Added form reset after submitting

--- src/components/contact/ContactForm.vue
+++ src/components/contact/ContactForm.vue
@@ -3,11 +3,11 @@
     <h1 class="mt-4 mb-4"><span id="guillemet">»</span> Kontaktieren Sie uns</h1>
     <form v-if="!emailSent && !error" class="form-group border rounded p-4 mt-4 w-50 mx-auto" ref="form" @submit.prevent="sendEmail">
         <label class="pt-2">Name</label>
-        <input class="form-control" type="text" name="user_name" placeholder="Vorname Nachname" required>
+        <input v-model="name" class="form-control" type="text" name="user_name" placeholder="Vorname Nachname" required>
         <label class="pt-2">E-Mail</label>
-        <input class="form-control" type="email" name="user_email" placeholder="vorname.nachname@gmail.com" required>
+        <input v-model="email" class="form-control" type="email" name="user_email" placeholder="vorname.nachname@gmail.com" required>
         <label class="pt-2">Nachricht</label>
-        <textarea class="form-control" name="message" placeholder="Ihre Nachricht an uns" required></textarea>
+        <textarea v-model="msg" class="form-control" name="message" placeholder="Ihre Nachricht an uns" required></textarea>
         <br>
         <input v-if="!emailSubmitted" class="btn btn-primary btn-cl" type="submit" value="Send">
         <div v-if="emailSubmitted" class="spinner-border" role="status">
@@ -29,21 +29,31 @@ import emailjs from '@emailjs/browser';
 export default {
   data() {
     return {
+        name: '',
+        email: '',
+        msg: '',
         emailSubmitted: false,
         emailSent: false,
         error: false
     }
   },
   methods: {
+    clearForm() {
+      this.name = '',
+      this.email = '',
+      this.msg = ''
+    },
     sendEmail() {
       this.emailSubmitted = true;
       emailjs.sendForm(process.env.VUE_APP_EMAILJS_SERVICE, process.env.VUE_APP_EMAILJS_TEMPLATE, this.$refs.form, process.env.VUE_APP_EMAILJS_KEY)
         .then((result) => {
             console.log('SUCCESS!', result.text);
             this.emailSent = true;
+            this.clearForm();
         }, (error) => {
             console.log('FAILED...', error.text);
             this.error = true;
+            this.clearForm();
         });
     }
   }
